6d681946deace3ef5a92c3688ad44b93c7f79dbb
Francis Potter <git@fpotter.com>
2013-08-02 16:59:41 -0700

    Fix to tuple_raw_input

--- tuple_raw_input.py
+++ tuple_raw_input.py
@@ -2,7 +2,7 @@ def tuple_raw_input():
     while True:
         my_input = raw_input()
         my_list = my_input.split(',')
-        if my_list[0].isdigit() and my_list[1].isdigit():
+        if len(my_list)==2 and my_list[0].isdigit() and my_list[1].isdigit():
             return ( int(my_list[0]) , int(my_list[1]) )
         print "Please enter (x),(y)"
 
